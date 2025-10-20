0c349eb5b0cd32d4e4ac04b7cb4061085dcef99a
nzehakujobi <nzehakujobi@gmail.com>
2025-10-20 11:08:30 +0100

    added files

--- Program.cs
+++ Program.cs
@@ -55,5 +55,10 @@ app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
