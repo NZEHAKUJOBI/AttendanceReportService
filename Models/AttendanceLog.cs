d927920b8f08427d6d3943634d2fa377a4b5fa7c
nzehakujobi <nzehakujobi@gmail.com>
2025-10-20 02:40:39 +0100

    fixed mapping error

--- Models/AttendanceLog.cs
+++ Models/AttendanceLog.cs
@@ -6,18 +6,43 @@ namespace AttendanceReportService.Models
     [Table("attendance_logs")]
     public class AttendanceLog
     {
-        public String Id { get; set; }
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("user_id")]
         public Guid UserId { get; set; }
+
+        [Column("full_name")]
         public string FullName { get; set; }
+
+        [Column("designation")]
         public string Designation { get; set; }
+
+        [Column("facility")]
         public string Facility { get; set; }
+
+        [Column("phone_number")]
         public string PhoneNumber { get; set; }
+
+        [Column("check_in_date")]
         public DateTime? CheckInDate { get; set; }
+
+        [Column("check_out_date")]
         public DateTime? CheckOutDate { get; set; }
+
+        [Column("check_in")]
         public DateTime? CheckIn { get; set; }
+
+        [Column("check_out")]
         public DateTime? CheckOut { get; set; }
+
+        [Column("message")]
         public string Message { get; set; }
+
+        [Column("success")]
         public bool Success { get; set; }
+
+        [Column("received_at")]
         public DateTime ReceivedAt { get; set; } = DateTime.UtcNow;
     }
 }
