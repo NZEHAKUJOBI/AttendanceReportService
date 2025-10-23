774b194cde4e0eaeb17c20927876b7636ad3e053
nzehakujobi <nzehakujobi@gmail.com>
2025-10-23 11:43:54 +0100

    add

---
+++ Migrations/20251023103837_AddUserIdToStaff.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttendanceReportService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSynced",
+                table: "staff");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "staff");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSynced",
+                table: "staff",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "staff",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
